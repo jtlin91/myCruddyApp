830e62eabf6353c4573eee538d24842c7011f481
unknown <jtlin91@gmail.com>
2018-06-06 18:18:49 -0700

    Completed code for add, search, and show all snippets

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 $(document).ready(function() {
 
+
     $('.setData').on('click', function() {
-      let textName = $('.textName').val();
-      let textCode = $('.textCode').val().replace(/\n/g, '<br>').replace(/ /g, '&nbsp');
+      let textName = $('.textName').val() + "**";
+      let textCode = $('.textCode').val();
+      //.replace(/\n/g, '<br>').replace(/ /g, '&nbsp');
       //$('.debug').text(textFieldValue);
       //.replace(/\n/g, '<br>')
-      localStorage.setItem('codeData', textCode);
-      localStorage.setItem('nameData', textName);
+      localStorage.setItem(textName, textCode);
       $('.textName').val('');
       $('.textCode').val('');
 
     });
 
     $('.getData').on('click', function() {
-      let retrievedName = localStorage.getItem('nameData');
-      let retrievedCode = localStorage.getItem('codeData');
-      $('.debug').html(retrievedName + "<br></br>" + "<code>" + retrievedCode + "</code>");
+      let textName = $('.textName').val() + "**";
+      let retrievedCode = localStorage.getItem(textName);
+      textName = textName.slice(0,-2);
+      $('.debug').html(textName + "<br></br>" + "<code>" + retrievedCode + "</code>");
+    });
+
+    $('.allData').on('click', function() {
+      for (var key in localStorage) {
+        if (key.includes("**")) {
+          let textName = key;
+          let retrievedCode = localStorage.getItem(textName);
+          textName = textName.slice(0,-2);
+          $('.debug').append("<div>" + textName + "<br></br>" + "<code>" + retrievedCode + "</code></div>");
+        }
+      }
     });
 
 
